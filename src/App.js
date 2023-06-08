d5e15f07cda90dd8ee749478036e1283aed41a12
vivek chhabra <vivekcb8642@gmail.com>
2023-06-08 20:44:58 +0530

    add App component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Game from "./Game";
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="App">
+            <Game />
+        </div>
+    );
+}
